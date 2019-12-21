1a9d658fa97c6b0d7f0594b75ce43628cbace9bc
Administrator <donchen1990@163.com>
2019-12-21 15:52:29 +0800

    MD5方法类库

--- Framework.CDQXIN.Utils/ExtensionHelper/DataTableExtHelper.cs
+++ Framework.CDQXIN.Utils/ExtensionHelper/DataTableExtHelper.cs
@@ -21,34 +21,34 @@ namespace Framework.CDQXIN.Utils.ExtensionHelper
         /// <typeparam name="T"></typeparam>
         /// <param name="dt">查询列表（不能是Excel文件的table）</param>
         /// <returns></returns>
-        public static List<T> ToList<T>(this DataTable dt)
-        {
-            //确认参数有效
-            List<T> list = new List<T>();
+        //public static List<T> ToList<T>(this DataTable dt)
+        //{
+        //    //确认参数有效
+        //    List<T> list = new List<T>();
 
-            if (dt == null || dt.Rows.Count == 0) return list;
+        //    if (dt == null || dt.Rows.Count == 0) return list;
 
-            for (int i = 0; i < dt.Rows.Count; i++)
-            {
-                //创建泛型对象
-                T t = Activator.CreateInstance<T>();
-                //获取对象所有属性
-                PropertyInfo[] propertyInfo = t.GetType().GetProperties();
-                for (int j = 0; j < dt.Columns.Count; j++)
-                {
-                    foreach (PropertyInfo info in propertyInfo)
-                    {
-                        //属性名称和列名相同时赋值
-                        if (dt.Columns[j].ColumnName.ToUpper().Equals(info.Name.ToUpper()))
-                        {
-                            info.SetValue(t, ChangeType(dt.Rows[i][j], info.PropertyType), null);
-                        }
-                    }
-                }
-                list.Add(t);
-            }
-            return list;
-        }
+        //    for (int i = 0; i < dt.Rows.Count; i++)
+        //    {
+        //        //创建泛型对象
+        //        T t = Activator.CreateInstance<T>();
+        //        //获取对象所有属性
+        //        PropertyInfo[] propertyInfo = t.GetType().GetProperties();
+        //        for (int j = 0; j < dt.Columns.Count; j++)
+        //        {
+        //            foreach (PropertyInfo info in propertyInfo)
+        //            {
+        //                //属性名称和列名相同时赋值
+        //                if (dt.Columns[j].ColumnName.ToUpper().Equals(info.Name.ToUpper()))
+        //                {
+        //                    info.SetValue(t, ChangeType(dt.Rows[i][j], info.PropertyType), null);
+        //                }
+        //            }
+        //        }
+        //        list.Add(t);
+        //    }
+        //    return list;
+        //}
 
         private static object ChangeType(object value, Type conversion)
         {
