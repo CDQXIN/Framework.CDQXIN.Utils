f5da98c18afaf0b7eff8feb514b1ec61418b37af
Administrator <donchen1990@163.com>
2019-12-21 16:50:26 +0800

    单例工具类

---
+++ Framework.CDQXIN.Utils/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.CDQXIN.Utils
+{
+    public sealed class Singleton<T> where T : new()
+    {
+        private static T _instance = new T();
+
+        private static readonly object lockHelper = new object();
+
+        private Singleton()
+        { }
+
+        public static T Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (lockHelper)
+                    {
+                        if (null == _instance)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void SetInstance(T value)
+        {
+            _instance = value;
+        }
+    }
+}
