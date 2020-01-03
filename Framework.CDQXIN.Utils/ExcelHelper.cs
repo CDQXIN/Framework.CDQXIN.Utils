f7352fc0c3a84fb7762bac5bdfa39c103a69938d
Administrator <donchen1990@163.com>
2020-01-03 09:40:26 +0800

    excel把第一个sheet放入datable中

--- Framework.CDQXIN.Utils/ExcelHelper.cs
+++ Framework.CDQXIN.Utils/ExcelHelper.cs
@@ -558,5 +558,37 @@ namespace Framework.CDQXIN.Utils
             }
             return ColsList;
         }
+
+        /// <summary>
+        /// 根据excle的路径把第一个sheet中的内容放入datatable
+        /// </summary>
+        /// <param name="path">excel存放的路径</param>
+        /// <returns></returns>
+        public static DataTable ReadExcelToTable(string path)
+        {
+            try
+            {
+
+                //连接字符串
+                string connstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';"; // Office 07及以上版本 不能出现多余的空格 而且分号注意
+                //string connstring = Provider=Microsoft.JET.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';"; //Office 07以下版本 
+                using (OleDbConnection conn = new OleDbConnection(connstring))
+                {
+                    conn.Open();
+                    DataTable sheetsName = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "Table" }); //得到所有sheet的名字
+                    string firstSheetName = sheetsName.Rows[0][2].ToString(); //得到第一个sheet的名字
+                    string sql = string.Format("SELECT * FROM [{0}]", firstSheetName); //查询字符串                    //string sql = string.Format("SELECT * FROM [{0}] WHERE [日期] is not null", firstSheetName); //查询字符串
+                    OleDbDataAdapter ada = new OleDbDataAdapter(sql, connstring);
+                    DataSet set = new DataSet();
+                    ada.Fill(set);
+                    return set.Tables[0];
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
     }
 }
